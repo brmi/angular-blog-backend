e440519aed02dd79c6d17baf73fd0a42210a6e29
brmi <desireelenart@gmail.com>
2018-02-09 16:54:25 -0800

    put default values in edit input boxes

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post, BlogService } from '../blog.service';
+
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
