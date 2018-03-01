45c02e8f562b0d56cf8648e0b1a5236cf3398a96
Melissa Chen <melissachen2010@hotmail.com>
2018-02-28 22:44:30 -0800

    Login Page html and routing

--- blog-server/routes/index.js
+++ blog-server/routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  console.log(req.query.username);
+  console.log(req.query.password);
+  res.render('login', { title: 'Login' });
+  // res.send('Response send to client::'+req.query.username);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
