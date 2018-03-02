d3dd3fae694ee983493beb06e3dbd0a670a9b404
brmi <desireelenart@gmail.com>
2018-03-02 01:35:09 -0800

    Revert "added dj.js"
    This reverts commit e5f32f0ca82a6266fa9756195f3bad9bcb70b60f.

--- blog-server/app.js
+++ blog-server/app.js
@@ -1,102 +1,55 @@
-var express = require('express');
+var db = require('./db')
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var blog = require('./routes/blog');
-
+var express = require('express');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+/* Connect to Mongo */ 
+mongoConnection = db.connectDB( function( err ) {
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  var index = require('./routes/index');
+  var users = require('./routes/users');
 
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
 
-// Register our Routes
-app.use('/', index);
-app.use('/users', users);
-app.use('/blog', blog);
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  app.use('/', index);
+  app.use('/users', users);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// Connect to database
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'BlogServer';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  findPosts(db, function() {
-    client.close();
-  })
-  findUsers(db, function() {
-    client.close();
-  })
-  client.close();
-});
-
-// Final All Posts
-const findPosts = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('Posts');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
   });
-}
 
-// Final All Users
-const findUsers = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('Users');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
   });
-}
+
+  console.log('Connected to Mongo!');
+});
 
 module.exports = app;
+
--- blog-server/routes/blog.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET blog listing. */
-router.get('/blog/:username', function(req, res, next) {
-    res.send(req.params);
-});
-
-module.exports = router;
